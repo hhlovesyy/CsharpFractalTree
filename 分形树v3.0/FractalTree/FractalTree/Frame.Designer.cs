c80ee7e762a735cae7bb9dc45dcd38a8086dcf7d
hhlovesyy <1596944152@qq.com>
2021-09-04 22:02:08 +0800

    Fractal Tree In Csharp

---
+++ 分形树v3.0/FractalTree/FractalTree/Frame.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace FractalTree
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+     
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 
+
+      
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            this.drawbt = new System.Windows.Forms.Button();
+            this.picturebox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.新建ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(627, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "递归深度";
+            // 
+            // nud1
+            // 
+            this.nud1.Location = new System.Drawing.Point(754, 65);
+            this.nud1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.nud1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nud1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(89, 25);
+            this.nud1.TabIndex = 1;
+            this.nud1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud1.ValueChanged += new System.EventHandler(this.nud1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(627, 232);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "树枝长度";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(754, 232);
+            this.tb1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(89, 25);
+            this.tb1.TabIndex = 5;
+            this.tb1.Text = "14";
+            this.tb1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(627, 144);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "角度设置";
+            // 
+            // nud3
+            // 
+            this.nud3.Location = new System.Drawing.Point(754, 144);
+            this.nud3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.nud3.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nud3.Name = "nud3";
+            this.nud3.Size = new System.Drawing.Size(89, 25);
+            this.nud3.TabIndex = 7;
+            this.nud3.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nud3.ValueChanged += new System.EventHandler(this.nud3_ValueChanged);
+            // 
+            // drawbt
+            // 
+            this.drawbt.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.drawbt.Location = new System.Drawing.Point(663, 301);
+            this.drawbt.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.drawbt.Name = "drawbt";
+            this.drawbt.Size = new System.Drawing.Size(180, 50);
+            this.drawbt.TabIndex = 8;
+            this.drawbt.Text = "画第一种分形树";
+            this.drawbt.UseVisualStyleBackColor = true;
+            this.drawbt.Click += new System.EventHandler(this.drawbt_Click);
+            // 
+            // picturebox
+            // 
+            this.picturebox.Location = new System.Drawing.Point(13, 57);
+            this.picturebox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.picturebox.Name = "picturebox";
+            this.picturebox.Size = new System.Drawing.Size(596, 510);
+            this.picturebox.TabIndex = 10;
+            this.picturebox.TabStop = false;
+            this.picturebox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新建ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 28);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 新建ToolStripMenuItem
+            // 
+            this.新建ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.另存为ToolStripMenuItem});
+            this.新建ToolStripMenuItem.Name = "新建ToolStripMenuItem";
+            this.新建ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.新建ToolStripMenuItem.Text = "新建";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.打开ToolStripMenuItem.Text = "打开";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click_1);
+            // 
+            // 另存为ToolStripMenuItem
+            // 
+            this.另存为ToolStripMenuItem.Name = "另存为ToolStripMenuItem";
+            this.另存为ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.另存为ToolStripMenuItem.Text = "另存为";
+            this.另存为ToolStripMenuItem.Click += new System.EventHandler(this.另存为ToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(663, 387);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "画第二种分形树";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 686);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picturebox);
+            this.Controls.Add(this.drawbt);
+            this.Controls.Add(this.nud3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nud1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nud3;
+        private System.Windows.Forms.Button drawbt;
+        private System.Windows.Forms.PictureBox picturebox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 新建ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存为ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 分形树v3.0/FractalTree/FractalTree/Frame.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace FractalTree
+{
+    
+    public partial class Form1 : Form
+    {
+        bool click1 = false;
+        bool click2 = false;
+        //文件名
+        public string curFileName= @"picture.jpg";
+        //图像对象
+        private System.Drawing.Bitmap curBitmap;
+        //public Bitmap bmp = new Bitmap(769, 650);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.Size = new Size(769, 650);
+            this.CenterToScreen();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            picturebox.BorderStyle = BorderStyle.Fixed3D;
+            this.Text = "分形树";
+            curBitmap = (Bitmap)Image.FromFile(curFileName);
+          
+            //Graphics g = Graphics.FromImage(bmp);
+            //picturebox.Image = ;
+            //curBitmap = (Bitmap)bmp;
+        }
+
+        public void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+
+            //Graphics g = this.CreateGraphics();
+            //picturebox.Refresh();
+            if (curBitmap != null)
+                {
+                //使用DrawImage的方法绘制图像
+                Graphics g = e.Graphics;
+                //curBitmap.Width, curBitmap.Height图像的宽度和高度
+                g.DrawImage(curBitmap, 0, 0, curBitmap.Width, curBitmap.Height);
+                }
+            if (click1)
+            {
+                //picturebox.Refresh();
+                //Graphics g = e.Graphics;
+                //if(curFileName!=null)
+                curBitmap = (Bitmap)Image.FromFile(curFileName);
+                //else
+                  //  curBitmap = bmp;
+                this.picturebox.Image = curBitmap;
+                Graphics g = Graphics.FromImage(this.picturebox.Image);
+                Pen pen = new Pen(Color.Black);
+                float length = Convert.ToSingle(tb1.Text) * 10;
+                double angle = Convert.ToDouble(nud3.Value);
+                int depth = Convert.ToInt32(nud1.Value);
+                float xo = (picturebox.Width / 2) - 2;
+                float yo = picturebox.Height - Convert.ToSingle(5.4);  
+                paintFractalTree(pen, xo, yo, depth, 90, length, angle, g);//那个90是确定这颗树往哪个方向开始长的
+                pen.Dispose();
+                click1 = false;
+            }
+            else if(click2)
+            {
+                //picturebox.Refresh();
+                if (curFileName != null)
+                    curBitmap = (Bitmap)Image.FromFile(curFileName);
+                //else
+                  //  curBitmap = bmp;
+                this.picturebox.Image = curBitmap;
+                Graphics g = Graphics.FromImage(this.picturebox.Image);
+                //Graphics g = e.Graphics;
+                Pen pen = new Pen(Color.Black);
+                float length = Convert.ToSingle(tb1.Text) * 10;
+                double angle = Convert.ToDouble(nud3.Value);
+                int depth = Convert.ToInt32(nud1.Value);
+                float xo = (picturebox.Width / 2) - 2;
+                float yo = picturebox.Height - Convert.ToSingle(5.4);
+                paintFractalTree2(pen, xo, yo, depth, 90, length*1/2, angle, g);//那个90是确定这颗树往哪个方向开始长的
+                pen.Dispose();
+                click2 = false;
+            }
+        }
+
+        private void paintFractalTree(Pen pen, float xo, float yo, int depth, double dangle, float length, double angle, Graphics line)
+        {
+            if (depth > -1)
+            {
+                length = length * 2 / 3;
+                float yf = yo - (sin(Converttohudu(dangle)) * length);//转换成弧度
+                float xf = xo - (cos(Converttohudu(dangle)) * length);
+                line.DrawLine(pen, xo, yo, xf, yf);
+                Random rnd = new Random();
+                // 随机色生成
+                Color myColor1 = Color.FromArgb
+                    (
+                         rnd.Next(0, 255), /*红色*/
+                         rnd.Next(0, 255), /*绿色*/
+                         rnd.Next(0, 255)  /*蓝色*/
+                    );
+
+                Pen pen2 = new Pen(myColor1);
+
+                paintFractalTree(pen2, xf, yf, depth - 1, dangle + angle, length, angle, line);//画右分支
+                Color myColor2 = Color.FromArgb
+                    (
+                         rnd.Next(0, 255), /*红色*/
+                         rnd.Next(0, 255), /*绿色*/
+                         rnd.Next(0, 255)  /*蓝色*/
+                    );
+                Pen pen3 = new Pen(myColor2);
+                
+                paintFractalTree(pen3, xf, yf, depth - 1, dangle - angle, length, angle, line);//画左分支
+            }
+        }
+        
+        private void paintFractalTree2(Pen pen, float xo, float yo, int depth, double dangle, float length, double angle, Graphics line)
+        {
+            double attach = 50;
+            if (depth > -1)
+            {
+                
+                float changelength = length * 2 / 3;
+                float x1 = xo - (cos(Converttohudu(dangle))) * length;
+                float y1 = yo- (sin(Converttohudu(dangle)))*length;
+                float x2 = x1 - (cos(Converttohudu(dangle))) * changelength;
+                float y2 = y1 - (sin(Converttohudu(dangle))) * changelength;
+                float x2r = x1 - (cos(Converttohudu(dangle+angle))) * changelength;
+                float y2r = y1 - (sin(Converttohudu(dangle+angle))) * changelength;
+                float x2l = x1 - (cos(Converttohudu(dangle -angle))) * changelength;
+                float y2l = y1 - (sin(Converttohudu(dangle - angle))) * changelength;
+                line.DrawLine(pen, xo, yo, x1, y1);
+                //line.DrawLine(pen, x1, y1, x2, y2);
+                line.DrawLine(pen, x1, y1, x2r, y2r);
+                //line.DrawLine(pen, x1, y1, x2l, y2l);
+                
+               
+                Random rnd = new Random();
+                // 随机色生成
+                Color myColor1 = Color.FromArgb
+                    (
+                         rnd.Next(0, 255), /*红色*/
+                         rnd.Next(0, 255), /*绿色*/
+                         rnd.Next(0, 255)  /*蓝色*/
+                    );
+
+                Pen pen2 = new Pen(myColor1);
+                paintFractalTree2(pen2, x2r, y2r, depth - 1, dangle + angle*3/2 + angle, length*2/3, angle, line);//画右分支
+                paintFractalTree2(pen2, x2r, y2r, depth - 1, dangle + angle * 3 / 2 - angle, length*2/3, angle, line);//画右分支
+                //paintFractalTree2(pen2, x2l, y2l, depth - 1, dangle - angle * 3 / 2 - angle, length*2/3, angle, line);//画右分支
+                //paintFractalTree2(pen2, x2l, y2l, depth - 1, dangle - angle * 3 / 2 + angle, length * 2 / 3, angle, line);//画右分支
+
+                Color myColor2 = Color.FromArgb
+                    (
+                         rnd.Next(0, 255), /*红色*/
+                         rnd.Next(0, 255), /*绿色*/
+                         rnd.Next(0, 255)  /*蓝色*/
+                    );
+                Pen pen3 = new Pen(myColor2);
+              
+            }
+        }
+
+        private float cos(double angle)
+        {
+            return (float)Math.Cos(angle);
+        }//求cos的值
+
+        private float sin(double angle)
+        {
+            return (float)Math.Sin(angle);
+        }//求sin的值
+
+        private double Converttohudu(double angle)
+        {
+            return (Math.PI * angle) / 180;
+        }//将角度转换成弧度
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar) && !e.KeyChar.Equals(','))
+            {
+                e.Handled = true;//可以从文本框中获取键盘读入的数据
+            }
+        }
+
+        private void drawbt_Click(object sender, EventArgs e)
+        {
+            click1 = true;
+            picturebox.Refresh();
+        }
+
+        private void nud3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nud1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opnDlg = new OpenFileDialog();
+            opnDlg.Filter = "所有图像文件 | *.bmp; *.pcx; *.png; *.jpg; *.gif;" +
+                "*.tif; *.ico; *.dxf; *.cgm; *.cdr; *.wmf; *.eps; *.emf|" +
+                "位图( *.bmp; *.jpg; *.png;...) | *.bmp; *.pcx; *.png; *.jpg; *.gif; *.tif; *.ico|" +
+                "矢量图( *.wmf; *.eps; *.emf;...) | *.dxf; *.cgm; *.cdr; *.wmf; *.eps; *.emf";
+            opnDlg.Title = "打开图像文件";
+            opnDlg.ShowHelp = true;
+            if (opnDlg.ShowDialog() == DialogResult.OK)
+            {
+                 curFileName = opnDlg.FileName;
+                try
+                {
+                    curBitmap = (Bitmap)Image.FromFile(curFileName);
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+            }
+            //对窗体进行重新绘制，这将强制执行paint事件处理程序
+            Invalidate();
+            picturebox.Refresh();
+
+
+        }
+        //private void Form1_Paint(object sender, PaintEventArgs e)
+        //{
+        //    //使用窗体的Paint事件的PaintEventArgs属性来获取一个与窗体相关联的Graphic对象。
+        //    Graphics g = e.Graphics;
+        //    if (curBitmap != null)
+        //    {
+        //        //使用DrawImage的方法绘制图像
+        //        //160,20 :显示在主窗体内，图像左上角的坐标
+        //        //curBitmap.Width, curBitmap.Height图像的宽度和高度
+        //        g.DrawImage(curBitmap, 160, 20, curBitmap.Width, curBitmap.Height);
+        //    }
+        //}
+
+
+        public void 保存ToolStripMenuItem_Click(object sender, EventArgs e ){ }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            click2 = true;
+            picturebox.Refresh();
+        }
+
+        private void 保存ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            
+            if (curBitmap == null)
+            {
+                
+                return;
+            }
+            Graphics g = Graphics.FromImage(curBitmap);
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Title = "保存为";
+            saveDlg.OverwritePrompt = true;
+            saveDlg.Filter =
+                "BMP文件 (*.bmp) | *.bmp|" +
+                "Gif文件 (*.gif) | *.gif|" +
+                "JPEG文件 (*.jpg) | *.jpg|" +
+                "PNG文件 (*.png) | *.png";
+            saveDlg.ShowHelp = true;
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveDlg.FileName;
+                string strFilExtn = fileName.Remove(0, fileName.Length - 3);
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "jpg":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case "tif":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void 另存为ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (curBitmap == null)
+            {
+
+                return;
+            }
+            Graphics g = Graphics.FromImage(curBitmap);
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Title = "保存为";
+            saveDlg.OverwritePrompt = true;
+            saveDlg.Filter =
+                "BMP文件 (*.bmp) | *.bmp|" +
+                "Gif文件 (*.gif) | *.gif|" +
+                "JPEG文件 (*.jpg) | *.jpg|" +
+                "PNG文件 (*.png) | *.png";
+            saveDlg.ShowHelp = true;
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveDlg.FileName;
+                string strFilExtn = fileName.Remove(0, fileName.Length - 3);
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "jpg":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "gif":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case "tif":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                    case "png":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    }
+
+
